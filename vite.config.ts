2e612fecd6992d2bbbbf46389450df3454e8e478
Andrey Kovalenko <andrey@bigcitylab.com>
2025-01-08 17:15:02 +0000

    fix: publish ts types

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 const LIB_NAME = "react-highlighter-words";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+  ],
   build: {
     lib: {
       entry: path.join(import.meta.dirname, "src/index.ts"),
