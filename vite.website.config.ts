946dfaf4f9cfeddf16875f848bed66686db015d8
Andrey Kovalenko <andrey@bigcitylab.com>
2025-01-08 14:24:44 +0000

    chore: configure github actions

--- vite.website.config.ts
+++ vite.website.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const LIB_NAME = "react-highlighter-words";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
